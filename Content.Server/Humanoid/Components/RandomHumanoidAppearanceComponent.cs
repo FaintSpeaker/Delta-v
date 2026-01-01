264b769a6938021c0a5377f576e22d9aac8a1d37
faintspeaker <joe@faintspeaker.com>
2026-01-01 13:49:20 -0500

    chore: comment clarification in the random humanoid appearance component

--- Content.Server/Humanoid/Components/RandomHumanoidAppearanceComponent.cs
+++ Content.Server/Humanoid/Components/RandomHumanoidAppearanceComponent.cs
@@ -14,21 +14,21 @@ public sealed partial class RandomHumanoidAppearanceComponent : Component
     /// After randomizing, sets the character's sex to this, if applicable.
     /// </summary>
     /// <remarks>
-    ///     Delta-V - Randomize Apperaance Functionality
+    ///     Delta-V - Support random appearances for spawned characters
     /// </remarks>
     [DataField] public Sex? Sex = null;
     /// <summary>
     /// After randomizing, sets the character's age to this, if applicable.
     /// </summary>
     /// <remarks>
-    ///     Delta-V - Randomize Apperaance Functionality
+    ///     Delta-V - Support random appearances for spawned characters
     /// </remarks>
     [DataField] public int? Age = null;
     /// <summary>
     /// After randomizing, sets the character's gender to this, if applicable.
     /// </summary>
     /// <remarks>
-    ///     Delta-V - Randomize Apperaance Functionality
+    ///     Delta-V - Support random appearances for spawned characters
     /// </remarks>
     [DataField] public Gender? Gender = null;
 
@@ -40,40 +40,43 @@ public sealed partial class RandomHumanoidAppearanceComponent : Component
     /// After randomizing, sets the character's hair color to this, if applicable.
     /// </summary>
     /// <remarks>
-    ///     Delta-V - Randomize Apperaance Functionality
+    ///     Delta-V - Support random appearances for spawned characters
     /// </remarks>
     [DataField] public Color? HairColor = null;
     /// <summary>
     /// After randomizing, sets the character's facial hair to this, if applicable.
     /// </summary>
     /// <remarks>
-    ///     Delta-V - Randomize Apperaance Functionality
+    ///     Delta-V - Support random appearances for spawned characters
     /// </remarks>
     [DataField] public string? FacialHair = null;
     /// <summary>
     /// After randomizing, sets the character's eye color to this, if applicable.
     /// </summary>
     /// <remarks>
-    ///     Delta-V - Randomize Apperaance Functionality
+    ///     Delta-V - Support random appearances for spawned characters
     /// </remarks>
     [DataField] public Color? EyeColor = null;
     /// <summary>
     /// After randomizing, sets the character's skin color to this, if applicable.
     /// </summary>
     /// <remarks>
-    ///     Delta-V - Randomize Apperaance Functionality
+    ///     Delta-V - Support random appearances for spawned characters
     /// </remarks>
     [DataField] public Color? SkinColor = null;
     /// <summary>
-    /// After randomizing, adds the markings from this dict, if applicable.
-    /// Will overwrite all randomized markings, if there are any.
-    /// Defined in YML as, for example:
+    /// If this dict is populated, all randomized markings will be replaced with these markings.
+    /// If the list is empty the color of that marking will be randomized.
+    /// </summary>
+    /// <example>
+    /// May be defined in YML files:
+    /// <code>
     /// markings:
     ///   ArachnidTorsoFiddleback: [ "#daf7da" ]
-    /// If the square brackets are empty (i.e. if the List<Color> has no members,) the color of that marking will be randomized.
-    /// </summary>
+    /// </code>
+    /// </example>
     /// <remarks>
-    ///     Delta-V - Randomize Apperaance Functionality
+    ///     Delta-V - Support random appearances for spawned characters
     /// </remarks>
     [DataField] public Dictionary<string, List<Color>>? Markings = null;
 }
